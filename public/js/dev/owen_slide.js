12b558db3207a63dc7470b732717fe44c948aeb8
owen Jeon <woonerfe@gmail.com>
2016-10-01 17:21:53 +0900

    다중 슬라이드 보강, 터치 이벤트 등록

--- public/js/dev/owen_slide.js
+++ public/js/dev/owen_slide.js
@@ -1,3 +1,6 @@
+ if (!Event.prototype.preventDefault) {Event.prototype.preventDefault=function() {this.returnValue=false;};}
+ if (!Event.prototype.stopPropagation) {Event.prototype.stopPropagation=function() {this.cancelBubble=true;};}
+
 $.fn.imagesLoaded = function (fn) {
 	var $imgs = this.find('img[src!=""]'), imgArr = {cpl : [], err : []};
 	if (!$imgs.length){
@@ -36,6 +39,8 @@ var Owen_slide = function(el, params){
 		initialSlide : 0,
 		activeW : 0,
 		slidesW : 0,
+		slidesToShow : 3,
+		loop : true,
 		autoplay : "",
 		easing : "swing",
 		mode : 'horizontal',
@@ -55,11 +60,19 @@ var Owen_slide = function(el, params){
 	var _this = this;
     this.bx = $(el);
     this.activeIdx = "";
-	var bx = this.bx, area = ( params.mode === 'horizontal' ? 'width' : 'height' ), dirc = ( params.mode === 'horizontal' ? 'left' : 'top' ) ,space = bx.css(area);
-	var oriLi = bx.find(params.list), oriLen = oriLi.length, ul = oriLi.parent(), interval = "";
-    var li, len, page = bx.find(params.paging), flag = true, activeSlide;
+	var bx = this.bx, area = ( params.mode === 'horizontal' ? 'width' : 'height' ), dirc = ( params.mode === 'horizontal' ? 'left' : 'top' ) ,space = parseInt(bx.css(area));
+	var oriLi = bx.find(params.list), oriLen = oriLi.length, ul = oriLi.parent(), interval = "", list;
+    var li, len, page = bx.find(params.paging), flag = true, activeSlide, win = $(window);
 	this.wrapper = ul[0], this.oriList = oriLi;
-	this.list = [];
+	list = this.list = [];
+	var sp_touch = this.support.touch;
+	var touchEv = {
+		down : (sp_touch ? 'touchstart' : 'mousedown'),
+		move : (sp_touch ? 'touchmove' : 'mousemove'),
+		up : (sp_touch ? 'touchend' : 'mouseup')
+	};
+
+
 
 	if (!window.getComputedStyle) {
 		window.getComputedStyle_MTslide = function (el, pseudo) {
@@ -82,17 +95,16 @@ var Owen_slide = function(el, params){
 
 
 	this.adjustW = function(active){
-		li.css(area, params.slidesW || space);
+		li.css(area, params.slidesW || (space/params.slidesToShow));
 		params.activeW && active.css(area, params.activeW);
 	};
-
 	this.slideTo = function(idx, speed, flg){
 		if(flg === undefined) flg = true;
-		var sd = this.list[idx];
+		var sd = typeof idx == 'object' ? idx : list[idx].el;
 		if(!flag) return;
 		flag = false;
 		speed = (speed === undefined) ? params.speed : speed;
-		var mo, sdIdx = sd.index(), dist = sd.position()[dirc] - (params.activeW && activeSlide && activeSlide.index() < sd.index()  ? params.activeW - params.slidesW : 0);
+		var mo, sdIdx = sd.index(), dist = sd.position()[dirc] - (params.activeW && activeSlide && activeSlide.index() < sd.index() ? params.activeW - params.slidesW : 0);
 		_this.activeIdx = sdIdx;
 		if(flg) params.onSlideChangeStart();
 		var aniEle = {};
@@ -116,8 +128,8 @@ var Owen_slide = function(el, params){
 
 		function aniCallBack(){
 			_this.setWrapperTransition(0);
-			if(sdIdx <= 1 || sdIdx >= len-2){ //솔루션화 하려면 이 부분을 좀더 보강해야 한다.
-				sd = sdIdx <= 1 ? _this.list[sdIdx+oriLen] : _this.list[sdIdx-oriLen];
+			if(list[sdIdx].duple){ //솔루션화 하려면 이 부분을 좀더 보강해야 한다.
+				sd = list[sdIdx].duple == -1 ? list[sdIdx+oriLen].el : list[sdIdx-oriLen].el;
 				dist = sd.position()[dirc] - (params.activeW && activeSlide && sdIdx <= 1  ?params.activeW - params.slidesW : 0 );
 				sdIdx = sd.index();
 				_this.adjustW(sd);
@@ -134,35 +146,132 @@ var Owen_slide = function(el, params){
 	this.slideNext = function(){_this.slideTo(_this.activeIdx+1)};
 	this.slidePrev = function(){_this.slideTo(_this.activeIdx-1)};
 
-	this.init = function(){
-		space = bx.css(area,'').css(area);
-		this.adjustW(_this.list[_this.activeIdx]);
+	this.reinit = function(){
+		space = parseInt(bx.css(area,'').css(area));
+		this.adjustW(list[_this.activeIdx].el);
 		this.slideTo(_this.activeIdx,0,false);
 	};
-	this.align = function(){
-		{ //솔루션화 하려면 이 부분을 좀더 보강해야 한다.
-			var addSlide = oriLi.eq(0).add(oriLi.eq(1)).clone().addClass('duplicate');
-			ul.append(addSlide);
-			addSlide = oriLi.eq(oriLen-1).add(oriLi.eq(oriLen-2)).clone().addClass('duplicate');
-			ul.prepend(addSlide);
+
+	this.resize = function(o){
+		o = o || {};
+		o.first || (space = parseInt(bx.css(area)), _this.adjustW(_this.activeIdx));
+		var wd = 0;
+		for(var j = 0 ; j < len ; j++){
+			wd += parseFloat(_this.list[j].el[0].style[area]);
 		}
+		ul.css(area, wd);
+		_this.slideTo(_this.activeIdx,0, false);
+
+	};
+	var x1, y1, x2, y2, mStart, mCheck, posi, moveDist, rectPoints = [];
+	this.swipeFn = {
+		down : function(e){
+			if(e.type == 'mousedown'){
+				x1 = e.pageX;
+				y1 = e.pageY;
+			}else{
+				x1 = e.originalEvent.touches[0].pageX;
+				y1 = e.originalEvent.touches[0].pageY;
+			}
+			posi = _this.getWrapperTranslate();
+			rectPoints = [bx.offset().top, bx.offset().left + bx.width(), bx.offset().top + bx.height(), bx.offset().left];
+			mStart = mCheck = true;
+			moveDist = 0;
+		},
+		move : function(e){
+			if(mStart){
+				if(e.type == 'mousemove'){
+					x2 = e.pageX - x1;
+					y2 = e.pageY - y1;
+				}else{
+					x2 = e.originalEvent.touches[0].pageX - x1;
+					y2 = e.originalEvent.touches[0].pageY - y1;
+				}
+				if(mCheck){
+					if((params.mode === 'horizontal' && (Math.abs(x2/y2) < 1 || Math.abs(x2/y2) == -Infinity)) || (params.mode !== 'horizontal' && (Math.abs(y2/x2) < 1 || Math.abs(y2/x2) == -Infinity))){
+						win.off(touchEv.move, _this.swipeFn.move);
+						win.off(touchEv.up, _this.swipeFn.up);
+						mCheck = false;
+						mStart = x1 = y1 = undefined;
+						return;
+					}
+					mCheck = false;
+				}
+				moveDist = params.mode === 'horizontal' ? x2 : y2
+				if (_this.support.transitions) {
+					_this.setWrapperTranslate(moveDist + posi);
+					_this.setWrapperTransition(0);
+				}
+				else {
+					ul.css(dirc, moveDist + posi);
+				}
+				if(e.pageY < rectPoints[0] || e.pageX > rectPoints[1] || e.pageY > rectPoints[2] || e.pageX < rectPoints[3]){
+					_this.swipeFn.up(e);
+				}
+			}
+			e.preventDefault();
+		},
+		up : function(e){
+			e.preventDefault();
+			mStart = x1 = y1 = undefined;
+			win.off(touchEv.move, _this.swipeFn.move);
+			win.off(touchEv.up, _this.swipeFn.up);
+			var dircFlag = moveDist > 0 ? -1 : 1;
+			if(100 < Math.abs(moveDist)){
+				var targetList = _this.activeIdx;
+				var bxLeft = bx.offset()[dirc];
+				for (var i = 0; i < list.length; i++) {
+					var liLeft = list[i].el.offset()[dirc];
+					if((liLeft - bxLeft) > 0 && dircFlag === 1) {
+						targetList = list[i].el;
+						break;
+					}else if((liLeft + list[i].el.innerWidth() - bxLeft) > 0 && dircFlag === -1){
+						targetList = list[i].el;
+						break;
+					}
+				}
+				_this.slideTo(targetList);
+			}else{
+				_this.slideTo(_this.activeIdx);
+			}
+		}
+	}
+
+
+	this.init = function(){
+		var addSlide = $(), preSlide = $();
+		for (var i = params.slidesToShow-1; i >= 0 ; i--) {
+			addSlide = addSlide.add(oriLi.eq(i));
+			preSlide = preSlide.add(oriLi.eq(oriLen-i-1));
+		}
+		ul.append(addSlide.clone().addClass('duplicate next'));
+		ul.prepend(preSlide.clone().addClass('duplicate prev'));
+
 		var initSlide = oriLi.eq(params.initialSlide);
 		li = bx.find(params.list), len = li.length;
 		var list = _this.list;
-		for(var i = 0 ; i < len ; i++) list.push(li.eq(i));
+		for(var i = 0 ; i < len ; i++) {
+			var l = li.eq(i);
+			list.push({el : l, duple : l.hasClass('duplicate prev') ? -1 : (l.hasClass('duplicate next') ? 1 : 0)});
+		}
 		len = list.length;
-		this.adjustW(initSlide);
-		$(li).imagesLoaded(function(){
-			var wd = 0;
-			for(var j = 0 ; j < len ; j++){
-				wd += parseInt(list[j].css(area));
-			}
-			ul.css(area, wd);
-			_this.slideTo(initSlide.index(),0, false);
+		this.activeIdx = initSlide.index();
+		this.adjustW(this.activeIdx);
+		li.imagesLoaded(function(){
+			_this.resize({first : 1});
+			win.off('resize',_this.resize).on('resize',_this.resize);
+
+			bx.on(touchEv.down,function(e){
+				if(!flag) return;
+				_this.swipeFn.down(e);
+				win.on(touchEv.move, _this.swipeFn.move);
+				win.on(touchEv.up, _this.swipeFn.up);
+			})
+
 			params.onLoad(_this);
 		});
 	};
-	this.align();
+	this.init();
 };//Owen_slide
 
 var motionObjs = {
@@ -201,7 +310,7 @@ var motionObjs = {
             matrix, curTransform, curStyle, transformMatrix;
 
         // automatic axis detection
-        if (typeof axis === 'undefined') {
+        if (axis === undefined) {
             axis = this.params.mode === 'horizontal' ? 'x' : 'y';
         }
 
@@ -282,111 +391,6 @@ var motionObjs = {
         'use strict';
         var es = this.wrapper.style;
         es.webkitTransitionDuration = es.MsTransitionDuration = es.msTransitionDuration = es.MozTransitionDuration = es.OTransitionDuration = es.transitionDuration = (duration / 1000) + 's';
-
-
-    },
-
-    /*==================================================
-        Helpers
-    ====================================================*/
-    h : {
-        getWidth: function (el, outer, round) {
-            'use strict';
-            var width = window.getComputedStyle_MTslide(el, null).getPropertyValue('width');
-            var returnWidth = parseFloat(width);
-            //IE Fixes
-            if (isNaN(returnWidth) || width.indexOf('%') > 0 || returnWidth < 0) {
-                returnWidth = el.offsetWidth - parseFloat(window.getComputedStyle_MTslide(el, null).getPropertyValue('padding-left')) - parseFloat(window.getComputedStyle_MTslide(el, null).getPropertyValue('padding-right'));
-            }
-            if (outer) returnWidth += parseFloat(window.getComputedStyle_MTslide(el, null).getPropertyValue('padding-left')) + parseFloat(window.getComputedStyle_MTslide(el, null).getPropertyValue('padding-right'));
-            if (round) return Math.ceil(returnWidth);
-            else return returnWidth;
-        },
-        getHeight: function (el, outer, round) {
-            'use strict';
-            if (outer) return el.offsetHeight;
-
-            var height = window.getComputedStyle_MTslide(el, null).getPropertyValue('height');
-            var returnHeight = parseFloat(height);
-            //IE Fixes
-            if (isNaN(returnHeight) || height.indexOf('%') > 0 || returnHeight < 0) {
-                returnHeight = el.offsetHeight - parseFloat(window.getComputedStyle_MTslide(el, null).getPropertyValue('padding-top')) - parseFloat(window.getComputedStyle_MTslide(el, null).getPropertyValue('padding-bottom'));
-            }
-            if (outer) returnHeight += parseFloat(window.getComputedStyle_MTslide(el, null).getPropertyValue('padding-top')) + parseFloat(window.getComputedStyle_MTslide(el, null).getPropertyValue('padding-bottom'));
-            if (round) return Math.ceil(returnHeight);
-            else return returnHeight;
-        },
-        getOffset: function (el) {
-            'use strict';
-            var box = el.getBoundingClientRect();
-            var body = document.body;
-            var clientTop  = el.clientTop  || body.clientTop  || 0;
-            var clientLeft = el.clientLeft || body.clientLeft || 0;
-            var scrollTop  = window.pageYOffset || el.scrollTop;
-            var scrollLeft = window.pageXOffset || el.scrollLeft;
-            if (document.documentElement && !window.pageYOffset) {
-                //IE7-8
-                scrollTop  = document.documentElement.scrollTop;
-                scrollLeft = document.documentElement.scrollLeft;
-            }
-            return {
-                top: box.top  + scrollTop  - clientTop,
-                left: box.left + scrollLeft - clientLeft
-            };
-        },
-        windowWidth : function () {
-            'use strict';
-            if (window.innerWidth) return window.innerWidth;
-            else if (document.documentElement && document.documentElement.clientWidth) return document.documentElement.clientWidth;
-        },
-        windowHeight : function () {
-            'use strict';
-            if (window.innerHeight) return window.innerHeight;
-            else if (document.documentElement && document.documentElement.clientHeight) return document.documentElement.clientHeight;
-        },
-        windowScroll : function () {
-            'use strict';
-            if (typeof pageYOffset !== 'undefined') {
-                return {
-                    left: window.pageXOffset,
-                    top: window.pageYOffset
-                };
-            }
-            else if (document.documentElement) {
-                return {
-                    left: document.documentElement.scrollLeft,
-                    top: document.documentElement.scrollTop
-                };
-            }
-        },
-
-        addEventListener : function (el, event, listener, useCapture) {
-            'use strict';
-            if (typeof useCapture === 'undefined') {
-                useCapture = false;
-            }
-
-            if (el.addEventListener) {
-                el.addEventListener(event, listener, useCapture);
-            }
-            else if (el.attachEvent) {
-                el.attachEvent('on' + event, listener);
-            }
-        },
-
-        removeEventListener : function (el, event, listener, useCapture) {
-            'use strict';
-            if (typeof useCapture === 'undefined') {
-                useCapture = false;
-            }
-
-            if (el.removeEventListener) {
-                el.removeEventListener(event, listener, useCapture);
-            }
-            else if (el.detachEvent) {
-                el.detachEvent('on' + event, listener);
-            }
-        }
     },
     setTransform : function (el, transform) {
         'use strict';
